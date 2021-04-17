be8b08761c4a0bc3de0e0701b190a96e1fd03466
Sohyun Ahn <sohyun_ahn@fitnyc.edu>
2021-04-18 01:45:35 +0900

    empty main page

--- src/com/algorithm/Main.java
+++ src/com/algorithm/Main.java
@@ -7,14 +7,5 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        for(int i = 0; i < N; i++) {
-            arrayList.add(sc.nextInt());
-        }
-        System.out.print(Collections.min(arrayList)+" "+Collections.max(arrayList));
     }
 }
